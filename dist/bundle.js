4d8ebd0d7912923523b90c83e8da162c546fc34d
Bohdan Petryshak <bohdanpv@gmail.com>
2018-01-20 15:15:45 +0200

    last commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,12 +73,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 class Messenger {
-    constructor(messagesBox, conversation) {
+    constructor(messagesBox, conversation, userImageLoctaion, form) {
         this.messageBox = messagesBox;
         this.conversation = conversation;
-        this.jsonAction = new __WEBPACK_IMPORTED_MODULE_0__JsonRequest__["a" /* default */]("dialog_1", "http://localhost:3000/");
-        this.inputField = document.getElementById('form');
-        this.button = document.getElementsByClassName('button')[0];
+        this.userImageLocation = userImageLoctaion;
+        this.jsonAction = new __WEBPACK_IMPORTED_MODULE_0__JsonRequest__["a" /* default */](conversation, "http://localhost:3000/");
+        this.inputField = form;
+        // this.button = document.getElementsByClassName('button')[0];
         this.phrases = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur excepturi in itaque nam nisi nulla odit perspiciatis",
                         "yoW!", "hey", "My name is Lew", "Really?O_O", "That`s amazing", "Ok", "Of course"];
     }
@@ -99,7 +100,8 @@ class Messenger {
             }
         });
         this.inputField.addEventListener('submit', this.saveUserMessage.bind(this));
-        this.button.addEventListener('click', this.botMessage.bind(this));
+        // this.button.addEventListener('click', this.botMessage.bind(this));
+        this.messageBox.scrollTo(0,this.messageBox.scrollHeight);
     }
     saveUserMessage(ev) {
             ev.preventDefault();
@@ -125,8 +127,8 @@ class Messenger {
 
     createUserMessageSection(messageObj) {
         let userMessagesBlockClass = (messageObj["isUser"]) ? "user-messages-block_user" : "user-messages-block_friend";
-        let imgObject = (messageObj["isUser"]) ? "" : '<object class="user-messages-block__friend-picture"' +
-            'type="image/png" data="img/friendImage.png"></object>';
+        let imgObject = (messageObj["isUser"]) ? "" : `<img class="user-messages-block__friend-picture"` +
+            `src="${this.userImageLocation}">`;
         let messageSection = `<div class="user-messages-block ${userMessagesBlockClass}"> ${imgObject}<ul></ul></div>`;
         let previousMessageSection = this.messageBox.lastElementChild;
         if(previousMessageSection) {
@@ -139,7 +141,7 @@ class Messenger {
     addMessageToMessageBlock(jsonMessageObj) {
         let classes = (jsonMessageObj['isUser']) ? "user-messages-block__message user-messages-block__message_user" :
             "user-messages-block__message user-messages-block__message_friend";
-        let  subMessage= `<li class="${classes}"><p>${jsonMessageObj['messageText']}</p></li>`;
+        let  subMessage= `<li class="${classes}"><p>${this.escapeHtml(jsonMessageObj['messageText'])}</p></li>`;
         let previousSubMessage = this.messageBox.lastElementChild;
         if(previousSubMessage) {
             previousSubMessage.insertAdjacentHTML('beforeend', subMessage);
@@ -167,13 +169,43 @@ class Messenger {
         form['message'].value = "";
     }
 
+
+
+    escapeHtml(unsafe) {
+        return unsafe
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
+
+
 }
 
 
 let messageBox = document.querySelector(".messages-section");
-let m = new Messenger(messageBox, 'dialog_1');
+let form = document.getElementsByClassName('form')[0];
+let m = new Messenger(messageBox, 'dialog_1',
+    "https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png",
+    form);
 m.render();
 
+let messageBox1 = document.querySelector(".messages-section_1");
+let form1 = document.getElementsByClassName('form')[1];
+let m1 = new Messenger(messageBox1, 'dialog_2',
+    'https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png',
+    form1);
+m1.render();
+
+let messageBox2 = document.querySelector(".messages-section_2");
+let form2 = document.getElementsByClassName('form')[2];
+let m2 = new Messenger(messageBox2, 'dialog_3',
+    'https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png',
+    form2);
+m2.render();
+// // console.log(messageBox1);
 
 /***/ }),
 /* 1 */
--- src/Messenger.js
+++ src/Messenger.js
@@ -1,12 +1,13 @@
 import JsonRequest from './JsonRequest';
 
 class Messenger {
-    constructor(messagesBox, conversation) {
+    constructor(messagesBox, conversation, userImageLoctaion, form) {
         this.messageBox = messagesBox;
         this.conversation = conversation;
-        this.jsonAction = new JsonRequest("dialog_1", "http://localhost:3000/");
-        this.inputField = document.getElementById('form');
-        this.button = document.getElementsByClassName('button')[0];
+        this.userImageLocation = userImageLoctaion;
+        this.jsonAction = new JsonRequest(conversation, "http://localhost:3000/");
+        this.inputField = form;
+        // this.button = document.getElementsByClassName('button')[0];
         this.phrases = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur excepturi in itaque nam nisi nulla odit perspiciatis",
                         "yoW!", "hey", "My name is Lew", "Really?O_O", "That`s amazing", "Ok", "Of course"];
     }
@@ -27,10 +28,8 @@ class Messenger {
             }
         });
         this.inputField.addEventListener('submit', this.saveUserMessage.bind(this));
-        this.button.addEventListener('click', this.botMessage.bind(this));
+        // this.button.addEventListener('click', this.botMessage.bind(this));
         this.messageBox.scrollTo(0,this.messageBox.scrollHeight);
-
-
     }
     saveUserMessage(ev) {
             ev.preventDefault();
@@ -56,8 +55,8 @@ class Messenger {
 
     createUserMessageSection(messageObj) {
         let userMessagesBlockClass = (messageObj["isUser"]) ? "user-messages-block_user" : "user-messages-block_friend";
-        let imgObject = (messageObj["isUser"]) ? "" : '<object class="user-messages-block__friend-picture"' +
-            'type="image/png" data="img/friendImage.png"></object>';
+        let imgObject = (messageObj["isUser"]) ? "" : `<img class="user-messages-block__friend-picture"` +
+            `src="${this.userImageLocation}">`;
         let messageSection = `<div class="user-messages-block ${userMessagesBlockClass}"> ${imgObject}<ul></ul></div>`;
         let previousMessageSection = this.messageBox.lastElementChild;
         if(previousMessageSection) {
@@ -70,7 +69,7 @@ class Messenger {
     addMessageToMessageBlock(jsonMessageObj) {
         let classes = (jsonMessageObj['isUser']) ? "user-messages-block__message user-messages-block__message_user" :
             "user-messages-block__message user-messages-block__message_friend";
-        let  subMessage= `<li class="${classes}"><p>${jsonMessageObj['messageText']}</p></li>`;
+        let  subMessage= `<li class="${classes}"><p>${this.escapeHtml(jsonMessageObj['messageText'])}</p></li>`;
         let previousSubMessage = this.messageBox.lastElementChild;
         if(previousSubMessage) {
             previousSubMessage.insertAdjacentHTML('beforeend', subMessage);
@@ -98,9 +97,40 @@ class Messenger {
         form['message'].value = "";
     }
 
+
+
+    escapeHtml(unsafe) {
+        return unsafe
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
+
+
 }
 
 
 let messageBox = document.querySelector(".messages-section");
-let m = new Messenger(messageBox, 'dialog_1');
+let form = document.getElementsByClassName('form')[0];
+let m = new Messenger(messageBox, 'dialog_1',
+    "https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png",
+    form);
 m.render();
+
+let messageBox1 = document.querySelector(".messages-section_1");
+let form1 = document.getElementsByClassName('form')[1];
+let m1 = new Messenger(messageBox1, 'dialog_2',
+    'https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png',
+    form1);
+m1.render();
+
+let messageBox2 = document.querySelector(".messages-section_2");
+let form2 = document.getElementsByClassName('form')[2];
+let m2 = new Messenger(messageBox2, 'dialog_3',
+    'https://res.cloudinary.com/demo/image/facebook/w_28,h_28,r_100/100009184192644.png',
+    form2);
+m2.render();
+// // console.log(messageBox1);
